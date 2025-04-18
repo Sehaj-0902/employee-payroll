14a62a6e6da3e60b534c55934060ef59e6926992
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-04-18 15:09:37 +0530

    [Sehaj] ADD: Setup and Tested Environments and Database

--- src/main/java/com/bridgelabz/employee_payroll/EmployeePayrollApplication.java
+++ src/main/java/com/bridgelabz/employee_payroll/EmployeePayrollApplication.java
@@ -3,14 +3,19 @@ package com.bridgelabz.employee_payroll;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
-		log.info("Employee Payroll App Started");
+		ApplicationContext context = SpringApplication
+				.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment",
+				context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ",
+				context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
